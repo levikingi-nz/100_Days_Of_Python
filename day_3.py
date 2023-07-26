1d0e7766c4bbd52bb2c8af7de9aaeb7d2b73fc23
Levi <levikingi.dev@gmail.com>
2023-07-26 13:26:36 +1200

    D3 Commit

--- day_3.py
+++ day_3.py
@@ -17,3 +17,53 @@ If the conditions are not true, then it will always default to the next line of 
   
 Indetation is a big key to the structure of your code, its defines the start and end of actions set in the indented block of code from top to bottom.
 """
+
+# Example 1 - Ticket Machine App
+
+"""
+print("Welcome to the rollercoaster!")
+height = int(input("What is your heigh in cm? "))
+
+if height >= 120:
+  print("You can ride the rollercoaster!")
+else:
+  print("Sorry, you have to grow taller before you can ride.")
+"""
+
+# Comparison Operators
+
+"""
+comparison operators are used to compare data, this can be useful to determine conditional statements.
+
+> = greater than symbol - determines if the value is greater than that of the object on the right-side of the symbol.
+< = lesser than symbol - determines if the value is lesser than that of the object on the right-side of the symbol.
+== = equal too - determines if both values are equal to eachother.
+
+- adding a equal symbol alongside a comparison operator gives it two outcomes. for example:
+
+if age >= 18:
+
+this askes if the age is (>) greater than or (=) equal too
+"""
+
+# Nested If/Elif Statements
+
+"""
+a nested if/elif statement is a if/else statement with more than one result, it is nested inside of the same indentation block as the first if statement hence the name nested if/elif statements.
+
+# Example - Rollercoaster Ticket App
+
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age in years? "))
+    if age < 12:
+        print("Your total fare is $5.")
+    elif age >= 18:
+        print("Your total fare is $12.")
+    else:
+        print("Your total fare is $7.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+"""
